5f6c65784ebfcb1113f56f05765b24d1cfaf8f77
Vitor Paulo <vitorblog12@gmail.com>
2026-08-30 00:28:01 -0300

    first commit

---
+++ src/test/java/dev/vitorpaulo/blog/BlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vitorpaulo.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
